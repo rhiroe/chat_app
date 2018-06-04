d84ea5b0b4b5c67f6e3dded558d579b34dfdafe0
Ryosuke Hiroe <ride.poke@gmail.com>
2018-06-04 11:37:04 +0900

    Finish open chat application

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,6 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "room_channel_#{params['room_id']}" #room_idを付け足して部屋を分ける[課題]
+    stream_from "room_channel_#{params['room_id']}"
   end
 
   def unsubscribed
@@ -8,6 +8,6 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)  # current_userを定義しないとNameErrorになる(current_userがchannelで使えない)
-    Message.create!(content: data['message'], user_id: current_user.id, room_id: params['room_id'])#ここにroom_idを取得[課題]
+    Message.create!(content: data['message'], user_id: current_user.id, room_id: params['room_id'])
   end
 end
