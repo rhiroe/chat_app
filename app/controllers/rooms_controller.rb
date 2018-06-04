533bc2378d51ecbf9268b9cda6d643e0543cd7f0
Ryosuke Hiroe <ride.poke@gmail.com>
2018-06-04 11:57:31 +0900

    All finish

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,15 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
 
+  def home
+    @rooms = Room.all
+  end
+
+  def new
+    Room.create()
+    redirect_to root_path
+  end
+
   def show
     @room = Room.find(params[:room_id])
     @messages = @room.messages
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'rooms#home'
+  get 'rooms/new'
   get 'rooms/:room_id' => 'rooms#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
