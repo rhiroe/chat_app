6285526d7d6e059a98459170e29049cee1c8ed22
eRy <ride.poke@gmail.com>
2018-06-06 21:15:10 +0900

    All finish

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :room, optional: true
+  validates :content, presence: true
 
   after_create_commit { MessageBroadcastJob.perform_later self }
 end
