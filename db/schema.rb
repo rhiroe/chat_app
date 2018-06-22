b888c8a1a6600713330ef2af27630c0c92420c1b
Ryosuke Hiroe <ride.poke@gmail.com>
2018-06-22 14:52:45 +0900

    Add user.name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_01_003832) do
+ActiveRecord::Schema.define(version: 2018_06_22_055205) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_06_01_003832) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
