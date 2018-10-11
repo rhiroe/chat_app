9e7fb12c67c32431bf99167ec2807e9386c10656
Ryosuke Hiroe <ride.poke@gmail.com>
2018-10-11 16:19:48 +0900

    Code improvements よくわからんとこにコメントで説明を追加 一覧取得時にorderを指定 current_userの取得方法を変更 warden_hook.rbの削除 jsファイルをcoffeeに変換

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 module ApplicationCable
+  # 接続しているユーザー ≒ current_user を定義する
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
 
@@ -8,14 +11,12 @@ module ApplicationCable
 
     private
 
-    def find_verified_user
-      verified_user = User.find_by(id: cookies.signed["user.id"])
-      if verified_user && cookies.signed['user.expires_at'] > Time.now
-        verified_user
-      else
+      def find_verified_user
+        session_key = cookies.encrypted[Rails.application.config.session_options[:key]]
+        verified_id = session_key['warden.user.user.key'][0][0]
+        verified_user = User.find_by(id: verified_id)
+        return verified_user if verified_user && cookies.signed['user.expires_at'] > Time.now
         reject_unauthorized_connection
       end
-    end
-
   end
 end
